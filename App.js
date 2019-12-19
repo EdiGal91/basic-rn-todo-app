31213d8c0271b3a9642188657f563a91a2d26a21
Edi Gal <edig@yad2.co.il>
2019-12-19 22:23:27 +0200

    remove g code

--- App.js
+++ App.js
@@ -2,19 +2,9 @@ import React from "react";
 import { StyleSheet, Text, Picker, View } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Picker></Picker>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+  return <View style={styles.container}></View>;
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
+  container: {}
 });
